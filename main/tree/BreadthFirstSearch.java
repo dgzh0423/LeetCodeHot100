d2de501765e37a3693a99e2e368ab1af44e443f1
donghao <1530444548@qq.com>
2024-04-25 21:31:02 +0800

    BFS框架解题

--- main/tree/BreadthFirstSearch.java
+++ main/tree/BreadthFirstSearch.java
@@ -3,27 +3,28 @@ package main.tree;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @author 15304
  */
 public class BreadthFirstSearch {
     /**
-     * BFS
+     * BFS求最大深度
      * @param root
-     * @return
+     * @return maxDepth
      */
     public int maxDepth(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        //初始化： 队列 queue （加入根节点 root ），层数 res = 0
+        // 初始化： 队列 queue （加入根节点 root ），深度 depth = 0
         List<TreeNode> queue = new LinkedList<>() , tmp;
         queue.add(root);
-        int res = 0;
+        int depth = 0;
 
         while (!queue.isEmpty()) {
-            //初始化一个空列表 tmp ，用于临时存储下一层节点
+            // 初始化一个空列表 tmp ，用于临时存储下一层节点
             tmp = new LinkedList<>();
             for(TreeNode node : queue) {
                 if (node.left != null) {
@@ -33,17 +34,18 @@ public class BreadthFirstSearch {
                     tmp.add(node.right);
                 }
             }
-            //遍历完一层，继续下一层
+            // 遍历完一层，继续下一层
             queue = tmp;
-            res++;
+            // 深度+1
+            depth++;
         }
-        return res;
+        return depth;
     }
 
     /**
-     * 二叉树层序遍历 （即逐层地，从左到右访问所有节点）
+     * BFS算法
      * @param root
-     * @return
+     * @return res
      */
     public List<List<Integer>> bfs(TreeNode root) {
         if (root == null) {
@@ -73,6 +75,44 @@ public class BreadthFirstSearch {
         }
         return res;
     }
+
+    /**
+     * BFS求最小深度
+     * @param root
+     * @return minDepth
+     */
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        // root 本身就是一层，depth 初始化为 1
+        int depth = 1;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            // 将当前队列中的所有节点向四周扩散
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                // 判断是否到达终点（也就是叶子节点）
+                if (cur.left == null && cur.right == null) {
+                    return depth;
+                }
+                /* 将 cur 的相邻节点加入队列 */
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            // 深度+1
+            depth++;
+        }
+        return depth;
+    }
+
 
     public static void main(String[] args) {
 
