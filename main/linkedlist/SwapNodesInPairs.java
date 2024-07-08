1192a3fadeac10d7eb44ac44795db66047dec74e
donghao <1530444548@qq.com>
2024-07-08 16:04:04 +0800

    24.两两交换链表中的节点

--- main/linkedlist/SwapNodesInPairs.java
+++ main/linkedlist/SwapNodesInPairs.java
@@ -1,7 +1,7 @@
 package main.linkedlist;
 
-
 /**
+ * 24.两两交换链表中的节点
  * @author 15304
  */
 public class SwapNodesInPairs {
@@ -13,17 +13,17 @@ public class SwapNodesInPairs {
      */
     public ListNode swapPairsRecursion(ListNode head) {
 
-        //终止条件：当前无节点或者只有一个节点，无法进行交换
+        // 终止条件：当前无节点或者只有一个节点，无法进行交换
         if (head == null || head.next == null){
             return head;
         }
 
         ListNode next = head.next;
-        //head连接后面交换完成的子链表，next连接head，完成交换
+        // head指向后面交换完成的子链表，next指向head，完成交换
         head.next = swapPairsRecursion(next.next);
         next.next = head;
 
-        //交换完成，next变为head
+        // 交换完成，next变为head
         return next;
 
     }
@@ -34,25 +34,23 @@ public class SwapNodesInPairs {
      * @return
      */
     public ListNode swapPairs(ListNode head) {
-        //初始化： 伪头节点 dum ，temp 指向 dum
+        // 初始化： 伪头节点 dum ，temp 指向 dum
         ListNode dum = new ListNode(0);
         dum.next = head;
         ListNode temp = dum;
         while(temp.next != null && temp.next.next != null) {
-            //start和end指向要交换的两个节点
+            // start和end指向要交换的两个节点
             ListNode start = temp.next;
             ListNode end = temp.next.next;
-            //交换过程
+            // 交换过程
             temp.next = end;
             start.next = end.next;
             end.next = start;
-            //交换完成，更新temp位置
+            // 交换完成，更新temp位置
             temp = start;
         }
         return dum.next;
     }
 
-    public static void main(String[] args) {
-
-    }
+    public static void main(String[] args) {}
 }
