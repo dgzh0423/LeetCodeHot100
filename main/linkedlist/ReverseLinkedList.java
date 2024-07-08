44c6c400e7605a1bc5cff69fb7bc813f025f37d0
donghao <1530444548@qq.com>
2024-07-08 15:03:13 +0800

    206.反转链表 修改递归解法

--- main/linkedlist/ReverseLinkedList.java
+++ main/linkedlist/ReverseLinkedList.java
@@ -1,6 +1,7 @@
 package main.linkedlist;
 
 /**
+ * 206.反转链表
  * @author 15304
  */
 public class ReverseLinkedList {
@@ -16,17 +17,16 @@ public class ReverseLinkedList {
         ListNode cur = head;
         ListNode tmp;
         while(cur != null) {
-            //记录当前节点的下一个节点
+            // 记录当前节点的下一个节点
             tmp = cur.next;
-            //然后将当前节点指向pre
+            // 然后将当前节点指向pre
             cur.next = pre;
-            //pre和cur节点都前进一位
+            // pre和cur节点都前进一位
             pre = cur;
             cur = tmp;
         }
         return pre;
     }
-
 
     /**
      * 递归法
@@ -34,21 +34,20 @@ public class ReverseLinkedList {
      * @return
      */
     public ListNode reverseListRecursion(ListNode head) {
-        //递归终止条件是当前为空，或者下一个节点为空
-        if(head==null || head.next==null) {
+        // 递归终止条件是当前为空，或者下一个节点为空
+        if(head == null || head.next == null) {
             return head;
         }
-        //这里的cur就是最后一个节点
-        ListNode cur = reverseList(head.next);
-        //如果链表是 1->2->3->4->5，那么此时的cur就是5
-        //而head是4，head的下一个是5，下下一个是空
-        //所以head.next.next 就是5->4
+        // 这里的cur就是最后一个节点
+        ListNode cur = reverseListRecursion(head.next);
+        // 如果链表是 1->2->3->4->5，那么此时的cur就是5
+        // 而head是4，head的下一个是5，下下一个是空
+        // 所以head.next.next = head 就是 5->4
         head.next.next = head;
-        //防止链表循环，需要将head.next设置为空
+        // 防止链表循环，需要将head.next设置为空
         head.next = null;
-        //每层递归函数都返回cur，也就是最后一个节点
+        // 每层递归函数都返回cur，也就是最后一个节点
         return cur;
-
     }
     public static void main(String[] args) {
     }
