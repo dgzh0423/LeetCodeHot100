c59b24e5013d9b28f5716a0a4ee03dac6b11343b
donghao <1530444548@qq.com>
2024-07-08 15:38:13 +0800

    234.回文链表

--- main/linkedlist/PalindromeLinkedList.java
+++ main/linkedlist/PalindromeLinkedList.java
@@ -1,12 +1,13 @@
 package main.linkedlist;
 
 /**
+ * 234.回文链表
  * @author 15304
  */
 public class PalindromeLinkedList {
 
     /**
-     *快慢指针法：快指针走到末尾，慢指针刚好到中间。其中慢指针将前半部分反转。然后比较
+     * 快慢指针法：快指针走到末尾，慢指针刚好到中间。其中慢指针将前半部分反转。然后比较
      * @param head
      * @return
      */
@@ -14,10 +15,11 @@ public class PalindromeLinkedList {
         if(head == null || head.next == null) {
             return true;
         }
-        //slow和fast分别用于走一半和走全程
+        // slow和fast分别用于走一半和走全程
         ListNode slow = head, fast = head;
-        //pre和prepre用于反转链表前一半
+        // pre和prepre用于反转链表前一半
         ListNode pre = head, prepre = null;
+        // 一边遍历，一边反转前半个链表
         while(fast != null && fast.next != null) {
             pre = slow;
             slow = slow.next;
@@ -25,11 +27,11 @@ public class PalindromeLinkedList {
             pre.next = prepre;
             prepre = pre;
         }
-        //if判断是为了处理链表个数为奇数的情况
+        // if判断是为了处理链表个数为奇数的情况
         if(fast != null) {
             slow = slow.next;
         }
-        //将反转后的前半链表与后半链表比较
+        // 将反转后的前半链表与后半链表比较
         while(pre != null && slow != null) {
             if(pre.val != slow.val) {
                 return false;
@@ -40,7 +42,5 @@ public class PalindromeLinkedList {
         return true;
     }
 
-    public static void main(String[] args) {
-
-    }
+    public static void main(String[] args) {}
 }
