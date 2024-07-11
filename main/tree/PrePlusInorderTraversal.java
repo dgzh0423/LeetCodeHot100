05995e968143377ef761027f329079128f273ad0
donghao <1530444548@qq.com>
2024-07-11 15:54:41 +0800

    105.根据前序和中序序列构造二叉树

--- main/tree/PrePlusInorderTraversal.java
+++ main/tree/PrePlusInorderTraversal.java
@@ -3,14 +3,14 @@ package main.tree;
 import java.util.HashMap;
 
 /**
- * 根据前序和中序序列构造二叉树
+ * 105.根据前序和中序序列构造二叉树
  * @author 15304
  */
 public class PrePlusInorderTraversal {
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        //因为preorder 和 inorder 均无重复元素
-        //故可用map将 inorder 的val值和对应的下标保存起来
+        // 因为preorder 和 inorder 均无重复元素
+        // 故可用map将 inorder 的val值和对应的下标保存起来，实现快速得到根节点（包括子树）下标
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < inorder.length; i++) {
             map.put(inorder[i], i);
@@ -18,37 +18,28 @@ public class PrePlusInorderTraversal {
         return buildTreeHelper(preorder, 0, preorder.length, inorder, 0, inorder.length, map);
     }
 
-    /**
-     * 左子树范围：[p_start, p_end)   [i_start, i_end)
-     *
-     * @param preorder
-     * @param p_start
-     * @param p_end
-     * @param inorder
-     * @param i_start
-     * @param i_end
-     * @param map
-     * @return
-     */
+    // [p_start, p_end) 和 [i_start, i_end) 都是左闭右开区间
     private TreeNode buildTreeHelper(int[] preorder, int p_start, int p_end, int[] inorder, int i_start, int i_end,
                                      HashMap<Integer, Integer> map) {
         if (p_start == p_end) {
             return null;
         }
-        //给定一个前序序列，第一个元素即为root
-        int root_val = preorder[p_start];
-        TreeNode root = new TreeNode(root_val);
-        int i_root_index = map.get(root_val);
-        // 左子树的node数
+        // 前序序列的第一个元素即为根节点
+        TreeNode root = new TreeNode(preorder[p_start]);
+        int i_root_index = map.get(root.val);
+        // 就可以确定左子树有多少节点
         int leftNum = i_root_index - i_start;
         // 递归构造左子树和右子树
         root.left = buildTreeHelper(preorder, p_start + 1, p_start  + 1 + leftNum, inorder, i_start, i_root_index, map);
         root.right = buildTreeHelper(preorder, p_start  + 1 + leftNum, p_end, inorder, i_root_index + 1, i_end, map);
-
+        // 返回根节点
         return root;
     }
 
     public static void main(String[] args) {
-
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+        TreeNode root = new PrePlusInorderTraversal().buildTree(preorder, inorder);
+        System.out.println(root.val);
     }
 }
