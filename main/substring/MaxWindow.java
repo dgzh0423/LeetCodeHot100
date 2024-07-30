c93ad33fc2472f67c0d55162fdba8b4e51c9fa34
donghao <1530444548@qq.com>
2024-07-30 10:19:49 +0800

    239. 滑动窗口最大值 修改注释

--- main/substring/MaxWindow.java
+++ main/substring/MaxWindow.java
@@ -4,8 +4,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 
 /**
- * 对于长度为 n 的数组 nums 而言，窗口的数量为 n−k+1
- * 对于两个相邻（只差了一个位置）的滑动窗口，它们共用着 k−1 个元素，而只有 1 个元素是变化的
+ * 239. 滑动窗口最大值
  * @author 15304
  */
 public class MaxWindow {
@@ -14,25 +13,23 @@ public class MaxWindow {
             return nums;
         }
 
-        // 双向队列 保存当前窗口最大值在数组中的位置index 保证队列中index按从大到小排序
+        // 双向队列 保存的是元素下标 i
         LinkedList<Integer> queue = new LinkedList<>();
-        // 结果数组
+        // 共有 n - k + 1 个滑动窗口
         int[] result = new int[nums.length-k+1];
         // 遍历nums数组
         for (int i = 0; i < nums.length; i++){
-            // 后面小于改成小于等于也可以
-            // 有等于的时候，提前让栈内相同的数出栈，就不会在判断queue.peek() < L 时出栈了
-            // 保证从大到小 如果前面数小则需要依次弹出，直至满足要求
+            // 保证下标i对应的元素按从大到小排序 如果前面数小则需要依次弹出，直至满足要求
             while (!queue.isEmpty() && nums[queue.peekLast()] < nums[i]){
                 queue.pollLast();
             }
             // 添加当前值对应的数组下标
             queue.offerLast(i);
-            // [L,R]实际上就是[i+1-k,i]，判断队首元素的位置是否小于L
+            // [L,R]实际上就是[i+1-k,i]，判断队首元素的下标是否在窗口内
             if (queue.peek() < i + 1 - k){
                 queue.poll();
             }
-            //当窗口长度为k时 保存当前窗口中最大值
+            // i = k - 1 时是第一个窗口，i++相当于窗口向右滑动一格
             if (i + 1 >= k){
                 result[i + 1 - k] = nums[queue.peek()];
             }
